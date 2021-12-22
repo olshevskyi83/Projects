5bf4820e3770fe29db5d321285c300ffcfe00a27
Marek <olshevskyi.projects@gmail.com>
2021-12-22 15:57:54 +0200

    traslade

--- JSprojects/app.js
+++ JSprojects/app.js
@@ -1,17 +1,22 @@
-const numberOffilms = +prompt('How many fils you watched?');
+const numberOfFilms = +prompt('How many fils you watched?');
 const personalMovieDB = {
-   count: numberOffilms,
+   count: numberOfFilms,
    movies: {},
    actors: {},
    genres: [],
    privat: false
 };
-const a = prompt('One of the last movie'),
-      b = prompt('film rate'),
-      c = prompt('One of the last movie'),
-      d = prompt('film rate');
 
-   personalMovieDB.movies [a] = b;
-   personalMovieDB.movies [c] = d;
-   console.log(personalMovieDB);
+ for (let i = 0; i < 2; i++) {
+   const a = prompt('One of the last movie'),
+         b = prompt('film rate');
+         
+   if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+      personalMovieDB.movies[a] = b;
+   } else {
+      console.log('try again');
+      i--;
+   }
+ }
+
           
