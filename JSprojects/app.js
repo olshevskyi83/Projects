be45b45a4ae417f2a11a400f2335e1a09bdf08fd
Marek <olshevskyi.projects@gmail.com>
2021-12-22 13:22:55 +0200

    'master'

---
+++ JSprojects/app.js
@@ -0,0 +1,27 @@
+// let num = +prompt('enter a number');
+// let sum = 0;
+// let tmp;
+
+
+// if (num < 10 || num > 99 || num == 0) {
+//     alert('This number is out of rules');
+// } else {
+//       while (num) {
+//       tmp = num % 10;
+//       num = (num - tmp) / 10;
+//       sum = tmp + num;
+//       alert(sum);
+//       break;
+//      }
+    
+// }
+var num = 5;
+
+while (num < 1000) {
+   num = num * 3;
+   
+   
+   
+  
+}
+console.log(num);
